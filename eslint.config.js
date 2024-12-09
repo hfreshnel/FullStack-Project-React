e64ffcd424bc42e48eca1ac65f4660376b8a6737
Hiroo <hiroo1mizuno@gmail.com>
2024-12-09 12:08:55 +0100

    add: ajout nouvelle branche

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: typescriptParser,
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
+      react: react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier: prettier,
+      '@typescript-eslint': typescriptEslint,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...typescriptEslint.configs.recommended.rules,
+
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    extends: ['plugin:prettier/recommended'],
   },
-)
+];
