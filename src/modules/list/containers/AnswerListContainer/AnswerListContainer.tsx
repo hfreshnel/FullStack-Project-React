03c136122b644e59dfe9248df4d70d272a6ceece
tisler <mathis.ranson@icloud.com>
2024-12-18 14:36:29 +0100

    [FEAT] Added answer list component

---
+++ src/modules/list/containers/AnswerListContainer/AnswerListContainer.tsx
@@ -0,0 +1,112 @@
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import IconButton from '../../../../common/components/icon-button/IconButton.tsx';
+import MainCard from '../../../../common/components/main-card/MainCard.tsx';
+import MainMenu from '../../../../common/components/main-menu/mainMenu.tsx';
+import TextButton from '../../../../common/components/text-button/TextButton.tsx';
+import { Color } from '../../../../common/enums/Color.ts';
+import { Size } from '../../../../common/enums/Size.ts';
+import './AnswerListContainer.css';
+
+const AnswerListContainer = ({
+  role = 'user',
+}: {
+  role?: 'admin' | 'user';
+}) => {
+  const Admin = role === 'admin';
+  return (
+    <div className={'question-list-container'}>
+      <MainMenu />
+      <div className={'titre'}>
+        <MainCard
+          label='Question 1'
+          textColor={Color.BLUE}
+          cardSize={Size.MEDIUM}
+          className='titre-nom'
+        ></MainCard>
+        <IconButton
+          icon={faPenToSquare}
+          iconColor={Color.YELLOW}
+          btnSize={Size.LARGE}
+          className='icon'
+        />
+      </div>
+      <div className={'question-list-container-content'}>
+        <MainCard
+          label=''
+          bgColor={Color.WHITE}
+          textColor={Color.BLUE}
+          className='contenu'
+        />
+        <MainCard
+          label='Réponse 1'
+          bgColor={Color.BLUE}
+          textColor={Color.WHITE}
+          className='choix'
+        />
+        {Admin && (
+          <IconButton
+            icon={faPenToSquare}
+            iconColor={Color.YELLOW}
+            bgColor={Color.WHITE}
+            className='edit'
+          />
+        )}
+        <MainCard
+          label='Réponse 2'
+          bgColor={Color.BLUE}
+          textColor={Color.WHITE}
+          className='choix'
+        />
+        {Admin && (
+          <IconButton
+            icon={faPenToSquare}
+            iconColor={Color.YELLOW}
+            bgColor={Color.WHITE}
+            className='edit'
+          />
+        )}
+        <MainCard
+          label='Réponse 3'
+          bgColor={Color.BLUE}
+          textColor={Color.WHITE}
+          className='choix'
+        />
+        {Admin && (
+          <IconButton
+            icon={faPenToSquare}
+            iconColor={Color.YELLOW}
+            bgColor={Color.WHITE}
+            className='edit'
+          />
+        )}
+        <MainCard
+          label='Réponse 4'
+          bgColor={Color.BLUE}
+          textColor={Color.WHITE}
+          className='choix'
+        />
+        {Admin && (
+          <IconButton
+            icon={faPenToSquare}
+            iconColor={Color.YELLOW}
+            bgColor={Color.WHITE}
+            className='edit'
+          />
+        )}
+      </div>
+
+      {Admin && (
+        <TextButton
+          label={'Sauvegarder la question '}
+          borderColor={Color.BLUE}
+          bgColor={Color.WHITE}
+          textColor={Color.BLUE}
+          btnSize={Size.LARGE}
+          className='button-sauvegarder'
+        />
+      )}
+    </div>
+  );
+};
+
+export default AnswerListContainer;
--- src/modules/list/routers/ListRouter/ListRouter.tsx
+++ src/modules/list/routers/ListRouter/ListRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import QuizListContainer from '../../containers/QuizListContainer/QuizListContainer.tsx';
 import QuestionListContainer from '../../containers/QuestionListContainer/QuestionListContainer.tsx';
+import AnswerListContainer from '../../containers/AnswerListContainer/AnswerListContainer.tsx';
 
 const ListRouter = () => {
   return (
     <Routes>
       <Route path='/quiz' element={<QuizListContainer />} />
       <Route path='/questions' element={<QuestionListContainer />} />
+      <Route path='/answer' element={<AnswerListContainer />} />
     </Routes>
   );
 };
